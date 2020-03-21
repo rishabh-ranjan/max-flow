b388606f98cad66b92ec7d0a66a6c669164978d0
rishabh-ranjan <mr.rishabh.ranjan@gmail.com>
2020-03-21 15:31:28 +0530

    fixed many issues, stuck at runtime error in laplacian!

--- src/max_flow.jl
+++ src/max_flow.jl
@@ -1,3 +1,7 @@
+using DataStructures
+using SparseArrays
+using Laplacians
+
 # types have been specified in the hopes of compiler optimization
 
 # internal representation of edges
@@ -25,18 +29,16 @@ mutable struct Graph
 	b::Float64			# max bottleneck in the graph
 end
 
-using DataStructures
-
 # sets maximum bottleneck of the graph and
 # sorts edges in descending order by capacity
 # return type void
 # complexity O(mlogm) dominated by sorting
 function set_max_bottleneck!(g::Graph)
 	sort!(g.edges, by = x -> -x.cap)
-	dsu = IntDisjointSets(g.n)
+	dsu = DataStructures.IntDisjointSets(g.n)
 	for e in g.edges
-		IntDisjointSets.union!(dsu, e.from, e.to)
-		if IntDisjointSets.in_same_sets(dsu, g.s, g.t)
+		DataStructures.union!(dsu, e.from, e.to)
+		if DataStructures.in_same_set(dsu, g.s, g.t)
 			g.b = e.cap
 			return
 		end
@@ -52,7 +54,7 @@ function equiv_graph!(g::Graph)
 	lb = g.eps*g.b/(2*g.m)
 	ub = g.m*g.b
 	cnt = g.m
-	for e in edges
+	for e in g.edges
 		if e.cap > ub
 			e.cap = ub
 		elseif e.cap < lb
@@ -70,7 +72,7 @@ end
 # returns false for "fail", else returns true
 # Note: flows get corrupted (these are no more needed anyway)
 # even when false is returned
-function oracle_1!(g::Graph, flow_val::Int64)
+function oracle_1!(g::Graph, flow_val::Float64)
 
 	# sum of wts
 	ws = 0.0
@@ -86,7 +88,8 @@ function oracle_1!(g::Graph, flow_val::Int64)
 	# sparse matrix has to be used as
 	# simple matrix construction and use would be O(n^2)
 	# dominating the intended complexity
-	lap = SparseArrays.spzeros(g.n, g.n)
+	#lap = SparseArrays.spzeros(g.n, g.n)
+	lap = zeros(g.n, g.n)
 	for e in g.edges
 		lap[e.from, e.to] = -e.cond
 		lap[e.to, e.from] = -e.cond
@@ -96,7 +99,8 @@ function oracle_1!(g::Graph, flow_val::Int64)
 
 	# construction of chi_s,t
 	# TODO: this need not be recomputed, move it out
-	chi = SparseArrays.spzeros(g.n)
+	#chi = SparseArrays.spzeros(g.n)
+	chi = zeros(g.n)
 	chi[g.s] = 1
 	chi[g.t] = -1
 
@@ -104,6 +108,7 @@ function oracle_1!(g::Graph, flow_val::Int64)
 	solver = Laplacians.KMPSDDMSolver(lap)
 	# vertex potentials
 	phi = solver(chi)
+	#phi = SparseArrays.spzeros(g.n)
 
 	for e in g.edges
 		e.flow = e.cond * (phi[e.to] - phi[e.from]) * flow_val
@@ -130,7 +135,7 @@ function mult_wt!(g::Graph, flow_val::Float64, oracle!::Function, rho::Float64)
 		e.acc_flow = 0.0
 	end
 	# number of iterations
-	n = ceil(Int, 2 * rho * max(log(m),1) / eps^2)
+	n = ceil(Int, 2 * rho * max(log(g.m),1) / g.eps^2)
 	for i in 1:n 
 		query = oracle!(g, flow_val)
 		if query == false
@@ -160,7 +165,7 @@ function binary_search_max_flow!(g::Graph, oracle!::Function, rho::Float64)
 	g.eps = g.eps - eps_bin		# TODO: actually g.eps should be some more for safety
 	while low + eps_bin*high < high
 		mid = (low + high)/2.0
-		query = mutl_wt!(g, mid, oracle!, rho)
+		query = mult_wt!(g, mid, oracle!, rho)
 		if query == false
 			high = mid
 		else
@@ -174,9 +179,9 @@ end
 # flow is modified in the graph itself
 # the graph itself is modified
 function compute_flow!(g::Graph)
-	set_max_bottleneck(g)
-	equiv_graph(g)
-	rho = 3 * sqrt(m/g.eps)
+	set_max_bottleneck!(g)
+	equiv_graph!(g)
+	rho = 3 * sqrt(g.m/g.eps)
 	oracle! = oracle_1!
 	binary_search_max_flow!(g, oracle!, rho)
 end
@@ -196,10 +201,10 @@ end
 
 function main()
 	# take input from stdin
-	eps = parse(Float64, split(readline()))
+	eps = parse(Float64, readline())
 	n, m = [parse(Int64, x) for x in split(readline())]
 	s, t = [parse(Int64, x) for x in split(readline())]
-	edges = Array{Edge, 1}(m)
+	edges = [Edge(0, 0, 0.0, 0.0, 0.0, 0.0, 0.0) for _ in 1:m]
 	for e in edges
 		inpl = split(readline())
 		e.from = parse(Int64, inpl[1])
@@ -214,7 +219,7 @@ function main()
 	# output the flows on the edges
 	# some edges may not be shown, such edges have flow 0
 	for e in g.edges
-		println(g.from, " ", g.to, " ", g.flow)
+		println(e.from, " ", e.to, " ", e.flow)
 	end
 	println()
 
